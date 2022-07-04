75963f50a64ee976dd7d07d3fcb0520ed05e0b9e
Sujan Adhikari <71167057+asujan205@users.noreply.github.com>
2022-07-04 22:55:07 +0545

    WebtokenReady

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -39,6 +39,38 @@ const registerUser=async(req,res)=>{
 	}
 
     }
+    const verifyToken=async(req,res)=>{
+        const token= req.headers['x-aces-token']
+        
+            try {
+                const decoded = jwt.verify(token, 'secret123')
+                const email = decoded.email
+                const user = await User.findOne({ email: email })
+        
+                return res.json({ status: 'ok', quote: user.quote })
+            } catch (error) {
+                console.log(error)
+                res.json({ status: 'error', error: 'invalid token' })
+            }
+        
+    }
+    const UpdateUser= async(req,res)=>{
+        const token = req.headers['x-access-token']
+
+	try {
+		const decoded = jwt.verify(token, 'secret123')
+		const email = decoded.email
+		await User.updateOne(
+			{ email: email },
+			{ $set: { quote: req.body.quote } }
+		)
+
+		return res.json({ status: 'ok' })
+	} catch (error) {
+		console.log(error)
+		res.json({ status: 'error', error: 'invalid token' })
+	}
+    }
 
 
 }
